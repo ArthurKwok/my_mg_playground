48658b345d88c8955b42364b2b229d496f284b66
Arthur Jinyue Guo <33652918+ArthurKwok@users.noreply.github.com>
2022-04-06 03:49:10 -0400

    Update transformer.py

--- models/transformer.py
+++ models/transformer.py
@@ -52,6 +52,7 @@ class PositionalEncoding(nn.Module):
                 self.pe[:, i] = torch.sin(pos / (10000**(2*i/n_dim)))
             else:
                 self.pe[:, i] = torch.cos(pos / (10000**(2*i/n_dim)))
+        self.pe = nn.Parameter(self.pe)
 
     def forward(self, x):
         # x: (..., n_seq, n_dim)
@@ -83,3 +84,41 @@ class TransformerDecoder(nn.Module):
         for layer in self.layers:
             x = layer(x, z)
         return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, n_seq, n_dim, n_head, n_layer, d_ff=2048):
+        super().__init__()
+        self.pe = PositionalEncoding(n_seq, n_dim)
+        self.encoder = TransformerEncoder(n_seq, n_dim, n_head, n_layer, d_ff)
+        self.decoder = TransformerDecoder(n_seq, n_dim, n_head, n_layer, d_ff)
+        self.ff = nn.Sequential(
+            nn.Linear(n_dim, d_ff),
+            nn.ReLU(),
+            nn.Linear(d_ff, n_dim)
+        )
+
+    def forward(self, x, x_dec):
+        # x, x_dec: (n_batch, n_seq, n_dim)
+        # x_dec is the shifted version of x
+        x = self.pe(x)
+        h = self.encoder(x)
+        output = self.decoder(self.pe(x_dec), h)
+
+        return output
+
+
+if __name__ == "__main__":
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    n_batch = 512
+    n_seq = 64
+    n_dim = 128
+    n_head = 8
+    n_layer = 2
+    input = torch.randn(n_batch, n_seq, n_dim).to(device)
+    input_shifted = torch.roll(input, -1, -2)
+    input_shifted[:, -1, :] = torch.zeros_like(input_shifted[:, -1, :])
+
+    tfm = Transformer(n_seq, n_dim, n_head, n_layer).to(device)
+    output = tfm(input, input_shifted)
+    print(output.shape)
